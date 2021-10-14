b17293672e38ee55d33ceca8d1953562baf04306
Justin Erasmus <82395096+BraveJustin@users.noreply.github.com>
2021-10-14 18:12:38 +0200

    Clean Up + Update endpoint update.
    Make code more friendly to linter's and for the copy + pasters.
    As well as update endpoint name update.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi import status
+from fastapi import FastAPI, status
 from fastapi.exceptions import HTTPException
 from models import Book
 from database import engine
@@ -12,18 +11,18 @@ app=FastAPI()
 session=Session(bind=engine)
 
 
-@app.get('/books',response_model=List[Book],
-status_code=status.HTTP_200_OK)
+@app.get('/books', response_model=List[Book],
+         status_code=status.HTTP_200_OK)
 async def get_all_books():
-    statement=select(Book)
-    results=session.exec(statement).all()
+    statement = select(Book)
+    results = session.exec(statement).all()
 
     return results
 
-@app.post('/books',response_model=Book,
-            status_code=status.HTTP_201_CREATED)
+@app.post('/books', response_model=Book,
+          status_code=status.HTTP_201_CREATED)
 async def create_a_book(book:Book):
-    new_book=Book(title=book.title,description=book.description)
+    new_book = Book(title=book.title, description=book.description)
 
     session.add(new_book)
 
@@ -33,11 +32,11 @@ async def create_a_book(book:Book):
     
     
 
-@app.get('/book/{book_id}',response_model=Book)
+@app.get('/book/{book_id}', response_model=Book)
 async def get_a_book(book_id:int):
-    statement=select(Book).where(Book.id==book_id)
+    statement = select(Book).where(Book.id==book_id)
 
-    result=session.exec(statement).first()
+    result = session.exec(statement).first()
 
     if result == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
@@ -47,14 +46,14 @@ async def get_a_book(book_id:int):
     
 
 
-@app.put('/book/{book_id}',response_model=Book)
-async def get_all_books(book_id:int,book:Book):
-    statement=select(Book).where(Book.id==book_id)
+@app.put('/book/{book_id}', response_model=Book)
+async def update_a_book(book_id:int,book:Book):
+    statement = select(Book).where(Book.id==book_id)
 
-    result=session.exec(statement).first()
+    result = session.exec(statement).first()
 
-    result.title=book.title
-    result.description=book.description
+    result.title = book.title
+    result.description = book.description
 
     session.commit()
 
@@ -63,16 +62,15 @@ async def get_all_books(book_id:int,book:Book):
 
     
 
-@app.delete('/book/{book_id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/book/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_a_book(book_id:int):
-    statement=select(Book).where(Book.id==book_id)
+    statement = select(Book).where(Book.id==book_id)
 
-    result=session.exec(statement).one_or_none()
+    result = session.exec(statement).one_or_none()
 
     if result == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-            detail="Resource Not Found"
-        )
+                            detail="Resource Not Found")
 
     session.delete(result)
 
